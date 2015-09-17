9fbb0a0ff473152f712613a12c3954a2c684b5a6
zenboss <zenyes@gmail.com>
2015-09-17 20:54:33 +0800

    fixed indent

--- index.js
+++ index.js
@@ -1,56 +1,56 @@
 /**
- * User:	zenboss
- * GitHub:	zenboss
- * Date:	2015-07-03
- * Time:	15:58
- * Email:	zenyes@gmail.com
+ * User: 	zenboss
+ * GitHub:  zenboss
+ * Date:    2015-07-03
+ * Time:    15:58
+ * Email:   zenyes@gmail.com
  */
 "use strict";
 
 var _innerNextTick = setImmediate;
 
 var workqueue = function(opt){
-	var self = this;
-	opt = opt||{};
-	if(!!opt.workMax)self.setWorkMax(opt.workMax);
-	self._queueArray = [];
-	self._queueNowRun = 0;
+    var self = this;
+    opt = opt||{};
+    if(!!opt.workMax)self.setWorkMax(opt.workMax);
+    self._queueArray = [];
+    self._queueNowRun = 0;
 
 };
 workqueue.create = function(opt){
-	return new workqueue(opt);
+    return new workqueue(opt);
 };
 workqueue.prototype.setQueueMax = workqueue.prototype.setWorkMax = function(maxnum){
-	this._workMax = parseInt(maxnum)||1000;
+    this._workMax = parseInt(maxnum)||1000;
 };
 
 workqueue.prototype.queue = function(workFn){
-	var self = this;
-	if('function' == typeof workFn){
-		self._queueArray.push(workFn);//将函数放入全局队列
-		self._doQueue();
-	}
+    var self = this;
+    if('function' == typeof workFn){
+        self._queueArray.push(workFn);//将函数放入全局队列
+        self._doQueue();
+    }
 
 };
 workqueue.prototype._doQueue = function(){
-	var self = this;
-	_innerNextTick(function(){
-		if(self._queueNowRun<self._workMax){
-			self._queueNowRun++;
-			var workFn = self._queueArray.pop();
+    var self = this;
+    _innerNextTick(function(){
+        if(self._queueNowRun<self._workMax){
+            self._queueNowRun++;
+            var workFn = self._queueArray.pop();
 
-			if(!!workFn){
-				workFn(function(){
-					self._queueNowRun--;
-					self._doQueue();
-				});
-			}else{
-				self._queueNowRun--;
-			}
+            if(!!workFn){
+                workFn(function(){
+                    self._queueNowRun--;
+                    self._doQueue();
+                });
+            }else{
+                self._queueNowRun--;
+            }
 
-		}
-	});
-	
+        }
+    });
+    
 };
 
 module.exports = workqueue;
--- test/test.js
+++ test/test.js
@@ -2,27 +2,30 @@
 
 var workqueue = require('../index.js').create({
 
-	workMax:10,//default 1000
+	workMax:100,//default 1000
 
 });
 
+var testFn = function(){
+	var i=0;
+	while(i<10000){
+		i++;
+		(function(){
+			var _i = i;
+
+			workqueue.queue(function(done){
+				console.log(arguments,_i,workqueue._queueNowRun);
+				// done();
+
+				setTimeout(done,_i%1000)
+			})
+		})();
+	}
+};
+testFn();
+
 
 var i=0;
-while(i<10000){
-	i++;
-	(function(){
-		var _i = i;
-
-		workqueue.queue(function(done){
-			console.log(arguments,_i,workqueue._queueNowRun);
-			// done();
-
-			setTimeout(done,_i%1000)
-		})
-	})();
-}
-
-i=0;
 while(false && i<10000){
 	i++;
 	(function(){
